cd66e087daad1526ca81c7177bc4c13afd5f1700
mr-suida <mr.suida@outlook.com>
2024-07-06 19:53:06 +0000

    Sum of Digits / Digital Root
    ...

---
+++ pyh3/sum_dig.py
@@ -0,0 +1,20 @@
+
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more
+# than one digit, continue reducing in this way until a single-digit
+# number is produced. The input will be a non-negative integer.
+
+# date: ??/??/???? ??:?? PM/AM
+# I'm: Normal?
+  
+# Runtime: ?? ms
+# Memory Usage: ?? MB
+
+def digital_root(number):
+    while number >= 10:
+        total = 0
+        while number > 0:
+            total += number % 10
+            number //= 10
+        number = total
+    return number
