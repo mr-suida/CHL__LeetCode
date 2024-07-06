b5baacecd777123531f4d646cb13fb652d318769
mr-suida <mr.suida@outlook.com>
2024-07-06 19:56:14 +0000

    Odd or Even?
    ...

---
+++ pyh3/odd_evn.py
@@ -0,0 +1,13 @@
+
+# Given a list of integers, determine whether the sum of its elements 
+# is odd or even. Give your answer as a string matching "odd" or "even".
+# If the input array is empty consider it as: [0] (array with a zero).
+
+# date: ??/??/???? ??:?? PM/AM
+# I'm: Normal?
+
+# Runtime: ?? ms
+# Memory Usage: ?? MB
+
+def odd_or_even(arr):
+    return 'odd' if sum(arr)%2 != 0 else 'even'
